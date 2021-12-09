134874fc0da8ea42e71d4fbfa180cc306f95a747
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-09 22:51:03 +0300

    Init

---
+++ src/components/app/app.tsx
@@ -0,0 +1,16 @@
+import {TextInput} from '@mantine/core';
+import React, {Suspense, lazy} from 'react';
+
+import {stylesContainer} from './app.module.less';
+import {stylesHeader, stylesImage} from './app.module.scss';
+
+const LazyStrawberryIcon = lazy(() => import('./strawberry'));
+export const App = (): React.ReactElement => (
+    <div className={stylesContainer}>
+        <div className={stylesHeader}>It works</div>
+        <TextInput />
+        <Suspense fallback={'loading...'}>
+            <LazyStrawberryIcon className={stylesImage} />
+        </Suspense>
+    </div>
+);
